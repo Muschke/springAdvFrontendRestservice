b8cffe60192d1afb9ff0286c052114cd0d441764
muskiGens <mussche.yannick@hotmail.be>
2022-03-08 10:53:06 +0100

    update8Maart

---
+++ index.js
@@ -0,0 +1,55 @@
+"use strict";
+const filialenUrl = "http://localhost:8080/filialen";
+leesFilialen();
+
+async function leesFilialen() {
+    const response = await fetch(filialenUrl);
+    if (response.ok) {
+        maakHyperlinksMet(await response.json());
+    } else {
+        technischeFout();
+    }
+}
+
+function maakHyperlinksMet(filialen) {
+    const ul = document.getElementById("filialen");
+    for(filiaal of filialen) {
+        ul.appendChild(maakLiMet(filiaal));
+    }
+}
+
+function maakLiMet(naam) {
+    const li = document.createElement("li");
+    const hyperlink = document.createElement("a");
+    hyperlink.href= "#";
+    //hyperlink.dataset.url = url;
+    //hyperlink.onclick = function () {
+    //    leesFiliaalMetUrl(this.dataset.url);
+    //};
+    li.appendChild(hyperlink);
+    return li;
+}
+
+
+/*function maakHyperlinksMet(filialen) {
+    const ul = document.getElementById("filialen");
+    for (const filiaal of filialen._embedded.filiaalIdNaamList) {
+        ul.appendChild(maakLiMet(filiaal.naam, filiaal._links.self.href));
+    }
+}
+
+function maakLiMet(naam, url) {
+    const li = document.createElement("li");
+    const hyperlink = document.createElement("a");
+    hyperlink.innerText = naam;
+    hyperlink.href = "#";
+    hyperlink.dataset.url = url;
+    hyperlink.onclick = function () {
+        leesFiliaalMetUrl(this.dataset.url);
+    };
+    li.appendChild(hyperlink);
+    return li;
+}*/
+function technischeFout() {
+    document.getElementById("technischeFout").hidden = false;
+}
