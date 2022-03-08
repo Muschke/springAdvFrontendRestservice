ae86a12f707ab200556f2cfae822283ca802defb
muskiGens <mussche.yannick@hotmail.be>
2022-03-08 13:20:50 +0100

    PUTrequest voor 1 attribuut te wijzigen

--- index.js
+++ index.js
@@ -14,42 +14,97 @@ async function leesFilialen() {
 function maakHyperlinksMet(filialen) {
     const ul = document.getElementById("filialen");
     for(var filiaal of filialen) {
-        ul.appendChild(maakLiMet(filiaal.naam));
+        ul.appendChild(maakLiMet(filiaal.naam, filiaal.id));
     }
 }
 
-function maakLiMet(naam) {
+function maakLiMet(naam, id) {
     const li = document.createElement("li");
     const hyperlink = document.createElement("a");
     hyperlink.href= "#";
-    //hyperlink.dataset.url = url;
-    //hyperlink.onclick = function () {
-    //    leesFiliaalMetUrl(this.dataset.url);
-    //};
-    li.appendChild(hyperlink);
-    return li;
-}
-
-
-/*function maakHyperlinksMet(filialen) {
-    const ul = document.getElementById("filialen");
-    for (const filiaal of filialen._embedded.filiaalIdNaamList) {
-        ul.appendChild(maakLiMet(filiaal.naam, filiaal._links.self.href));
-    }
-}
-
-function maakLiMet(naam, url) {
-    const li = document.createElement("li");
-    const hyperlink = document.createElement("a");
     hyperlink.innerText = naam;
-    hyperlink.href = "#";
-    hyperlink.dataset.url = url;
+    hyperlink.dataset.url = "http://localhost:8080/filialen/" + id;
     hyperlink.onclick = function () {
         leesFiliaalMetUrl(this.dataset.url);
     };
     li.appendChild(hyperlink);
     return li;
-}*/
+}
+
+/*om de aangeklikte filialen te laden*/
+async function leesFiliaalMetUrl(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        toonDetailVan(await response.json());
+    } else {
+        technischeFout();
+    }
+}
+function toonDetailVan(filiaal) {
+    document.getElementById("id").innerText = filiaal.id;
+    document.getElementById("naam").innerText = filiaal.naam;
+    document.getElementById("gemeente").innerText = filiaal.gemeente;
+    document.getElementById("omzet").innerText = filiaal.omzet;
+}
+/* OEF: veld in detail aangeklikte filiaal om omzet up te daten:*/
+document.getElementById("omzetWijzigen").onclick = wijzigen;
+async function wijzigen() {
+    const Filaal = {
+        naam:,
+        gemeente:,
+        omzet: document.getElementById("gewijzigdeOmzet").value,
+    };
+    try{
+        const response = await fetch(filialenUrl,
+            {method: "POST", headers: {"content-type": "application/json"},
+                body: JSON.stringify(omzetFiliaal)});
+        if(response.ok) {
+            document.getElementById("technischeFout").hidden = true;
+        } else {
+            technischeFout();
+        }
+    } catch {
+        technischeFout();
+    }
+}
+
+/*om filiaal te kunnen toevoegen*/
+document.getElementById("toevoegen").onclick = toevoegen;
+async function toevoegen() {
+    const verkeerdeElementen = document.querySelectorAll("input:invalid");
+    for (const element of verkeerdeElementen) {
+        document.getElementById(`${element.id}Fout`).hidden = false;
+    }
+    const correcteElementen = document.querySelectorAll("input:valid");
+    for (const element of correcteElementen) {
+        document.getElementById(`${element.id}Fout`).hidden = true;
+    }
+    if (verkeerdeElementen.length === 0) {
+        const filiaal = {
+            naam: document.getElementById("nieuweNaam").value,
+            gemeente: document.getElementById("nieuweGemeente").value,
+            omzet: document.getElementById("nieuweOmzet").value
+        };
+        try {
+            const response = await fetch(filialenUrl,
+                {method: "POST", headers: {"content-type": "application/json"},
+                    body: JSON.stringify(filiaal)});
+            if (response.ok) {
+                const ul = document.getElementById("filialen");
+                const li = maakLiMet(filiaal.naam, response.headers.get("Location"));
+                ul.appendChild(li);
+                document.getElementById("technischeFout").hidden = true;
+            } else {
+                technischeFout();
+            }
+        } catch {
+            technischeFout();
+        }
+    }
+}
+
+
+/*weergeven/verbergen technische fout*/
 function technischeFout() {
     document.getElementById("technischeFout").hidden = false;
 }
