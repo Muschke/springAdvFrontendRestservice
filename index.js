47de7548352e2a8fae5063b8152282d95ca24856
muskiGens <mussche.yannick@hotmail.be>
2022-03-15 09:55:11 +0100

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function maakHyperlinksMet(filialen) {
 function maakLiMet(naam, id) {
     const li = document.createElement("li");
     const hyperlink = document.createElement("a");
-    hyperlink.href= "http://localhost:63342/frontendRestservice/detail.html";
+    hyperlink.href= "http://localhost:63342/frontendRestservice/detail/"+id;
     hyperlink.target = "_blank";
     hyperlink.innerText = naam;
    // hyperlink.dataset.url = "http://localhost:8080/filialen/" + id;
